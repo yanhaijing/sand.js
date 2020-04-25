27c63dddbf32eab1ad73e437f52ed8cc78933745
yanhaijing <yanhaijing@yeah.net>
2020-04-25 22:04:24 +0800

    feat: add 空元素 demo

--- demo/demo-jsx/index.jsx
+++ demo/demo-jsx/index.jsx
@@ -1,10 +1,21 @@
+class HelloMessage2 extends Component {
+    render() {
+        return (
+            <>
+                <div>Hello {this.props.name}</div>
+            </>
+        );
+    }
+}
 class HelloMessage extends Component {
     render() {
-        return <div>Hello {this.props.name}</div>;
+        return (
+            <div>
+                Hello {this.props.name}
+                <HelloMessage2></HelloMessage2>
+            </div>
+        );
     }
 }
 
-render(
-    <HelloMessage name="Taylor" />,
-    document.getElementById('container')
-);
+render(<HelloMessage name="Taylor" />, document.getElementById('container'));
